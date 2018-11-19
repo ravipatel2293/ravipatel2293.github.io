3cfb5236c2bd2b156a7448861a3309f7c4921388
ravipatel1 <ravipatel@auberginesolutions.com>
2018-11-19 18:03:22 +0530

    create repso

---
+++ precache-manifest.b160d0892323e62774e5e9d4b7fe713c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/static/media/FontsFree-Net-proxima_nova_reg-webfont.feface72.ttf"
+  },
+  {
+    "revision": "284561febf8d2608625142c4c5a0be05",
+    "url": "/static/media/FontsFree-Net-Proxima-Nova-Thin.284561fe.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/static/media/FontsFree-Net-Proxima-Nova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e31282ff7533bee587f7",
+    "url": "/static/js/main.e31282ff.chunk.js"
+  },
+  {
+    "revision": "5a9d1224e4c2c7858c7a",
+    "url": "/static/js/1.5a9d1224.chunk.js"
+  },
+  {
+    "revision": "e31282ff7533bee587f7",
+    "url": "/static/css/main.c8a8b6c5.chunk.css"
+  },
+  {
+    "revision": "5a9d1224e4c2c7858c7a",
+    "url": "/static/css/1.75485e25.chunk.css"
+  },
+  {
+    "revision": "c6571fedadc17e7544b5800c2a0e2c32",
+    "url": "/index.html"
+  }
+];
